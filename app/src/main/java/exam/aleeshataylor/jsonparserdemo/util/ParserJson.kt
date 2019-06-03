4c18700fdebfc09bd94e70684710be62840f6113
AeeshaTaylor <jiaminChan98@gmail.com>
2019-06-03 18:00:41 +0800

    JSON解析--每日一句

--- app/src/main/java/exam/aleeshataylor/jsonparserdemo/util/ParserJson.kt
+++ app/src/main/java/exam/aleeshataylor/jsonparserdemo/util/ParserJson.kt
@@ -3,8 +3,9 @@ package exam.aleeshataylor.jsonparserdemo.util
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import exam.aleeshataylor.jsonparserdemo.entity.School
+import exam.aleeshataylor.jsonparserdemo.entity.Sentence
 import org.json.JSONArray
-import java.util.*
+import org.json.JSONObject
 
 class ParserJson {
     /**
@@ -30,6 +31,23 @@ class ParserJson {
     }
 
     /**
+     * 解析每日一句的JSON字符串
+     */
+    fun parserArrayForSentence(jsonArr: String): List<Sentence> {
+        val sentences = ArrayList<Sentence>()
+        val array = JSONArray(jsonArr)
+        for (i in 0 until array.length()) {
+            val obj: JSONObject = array.getJSONObject(i)
+            val s = Sentence(
+                obj.getString("phrase"),
+                obj.getString("author")
+            )
+            sentences.add(s)
+        }
+        return sentences
+    }
+
+    /**
      * 使用GSON解析JSON数组
      * @param jsonArr JSON对象数组字符串
      * @return 对应的对象集合
@@ -38,6 +56,13 @@ class ParserJson {
         val gson = Gson()
         //利用反射机制，得到List<Person>的类型
         val type = object : TypeToken<List<School>>() {}.type
+        return gson.fromJson(jsonArr, type)
+    }
+
+    fun parserArrayToSentence(jsonArr: String): List<Sentence>{
+        val gson = Gson()
+        //利用反射机制，得到List<Sentence>的类型
+        val type = object : TypeToken<List<Sentence>>() {}.type
         return gson.fromJson(jsonArr, type)
     }
 }
--- app/src/main/java/exam/aleeshataylor/jsonparserdemo/view/DailySentenceActivity.kt
+++ app/src/main/java/exam/aleeshataylor/jsonparserdemo/view/DailySentenceActivity.kt
@@ -1,13 +1,66 @@
 package exam.aleeshataylor.jsonparserdemo.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import exam.aleeshataylor.jsonparserdemo.R
+import android.util.Log
+import exam.aleeshataylor.jsonparserdemo.entity.Sentence
+import exam.aleeshataylor.jsonparserdemo.util.GetJsonUtil
+import exam.aleeshataylor.jsonparserdemo.util.ParserJson
+import kotlinx.android.synthetic.main.activity_daily_sentence.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class DailySentenceActivity : AppCompatActivity() {
 
+    @SuppressLint("SimpleDateFormat", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_daily_sentence)
+        setContentView(exam.aleeshataylor.jsonparserdemo.R.layout.activity_daily_sentence)
+
+        //获取系统时间
+        val format = SimpleDateFormat("HH:mm:ss")
+        val date = Date()
+        val time = format.format(date)
+
+
+        //获取零点整
+        val nowTime = System.currentTimeMillis()
+        val todayStartTime = nowTime - (nowTime + TimeZone.getDefault().rawOffset) % (1000 * 3600 * 24)
+        val tagTime = format.format(todayStartTime)
+        Log.i("时间",tagTime)
+
+        //获得每日一句的JSON数据的字符串对象
+        val jsonArr: String = GetJsonUtil.getJson("sentence.json",this)
+        //实例化ParserJson类
+        val parserJson = ParserJson()
+        //获得每日一句的JSON数据的数组对象
+        val ss: List<Sentence> = parserJson.parserArrayForSentence(jsonArr)
+        txt_daily.text = ss[0].toString()
+
+//        val ss: List<Sentence> = parserJson.parserArrayToSentence(jsonArr)
+//        showJSONArrayData(ss)
+
+        if (time == tagTime){
+            for (i in 0 until ss.size) {
+                if (ss[i] === txt_daily.text) {
+                    txt_daily.text = ss[i+1].toString()
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 显示JSON数组的值
+     * @param type 当前用什么类型解析
+     * @param ps 要显示数据的对象数组
+     */
+    @SuppressLint("SetTextI18n")
+    fun showJSONArrayData(ss: List<Sentence>) {
+        for (s in ss) {
+            txt_daily.append(s.toString() + "\n")
+        }
     }
 }
